4f82e2c66ddd82d8e84fed9c542faa79984560bd
Lee <elcheveremochilero@gmail.com>
2023-05-20 18:37:46 -0500

    New components

---
+++ src/components/Results/Results.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+interface Props {
+    results : string[]
+}
+
+const Results = ({results}:Props) => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            
+            {results.length >= 8 && <Link href=''>More contents: </Link>}
+        </div>
+    );
+}
+ 
+export default Results;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,11 @@ interface Props {
 
 export default function Home({photos}: Props) {
   return (
-    <div className="flex flex-col gap-10 items-center justify-center">
+    <div className="flex flex-col gap-10 items-center justify-center m-4">
       <h1>Instagallery</h1>
-      <h2>Search your favorite pics here</h2>
+      <h3>Search your favorite pics here</h3>
       <SearchBar/>
+      <p>Results:</p>
     </div>
   )
 }
