9f06d7f184d11767064af1f8706936b493cf9445
Lee <elcheveremochilero@gmail.com>
2023-05-28 13:33:10 -0500

    login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,37 @@
-const Login = () => {
-  return <h1>Login</h1>;
+import { FormEvent, useState } from "react";
+import Button from "@/components/Button/Button";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e;
+    // Perform login logic here
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Login</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border border-gray-300 px-4 py-2 rounded"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border border-gray-300 px-4 py-2 rounded"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
