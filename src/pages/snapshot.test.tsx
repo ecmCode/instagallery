0f8d08ff4cdba2bfacd83dec5858272b0525d85d
Lee <elcheveremochilero@gmail.com>
2023-06-05 19:30:07 -0500

    fix error

--- src/pages/snapshot.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "./";
-
-describe("Home", () => {
-  it("matches the snapshot", () => {
-    const { asFragment } = render(<Home photos={[]} />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
